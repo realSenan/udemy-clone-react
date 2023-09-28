72a34c93dd1d5c3d5b6ba614d115f62934ecda78
Senan <senanm260@gmail.com>
2023-09-29 01:21:57 +0400

    change range card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,7 +14,7 @@ const Cards = ({ product }) => {
 
         console.log(offset - item);
 
-        if (offset - item < 515) {
+        if (offset - item < 530) {
             setLeft("-23.75rem");
         } else {
             setLeft("13.75rem");
